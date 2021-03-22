7364ec11681a612e04de0e9104c38a11a9aaa35f
HyeonWooJung <1995hyunwoo@gmail.com>
2021-03-22 21:55:38 +0900

    dsd

--- pages/_document.js
+++ pages/_document.js
@@ -14,17 +14,17 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta name={`description`} content={`The Soul Card Project`}/>
           <meta name={'keywords'} content={`soul card`}/>
-          <!-- Google Tag Manager -->
+
           <script src={'/js/gtm.js'}></script>
-          <!-- End Google Tag Manager -->
+
         </Head>
         <body>
           <Main />
           <NextScript />
-          <!-- Google Tag Manager (noscript) -->
+
           <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WF3BWH2"
                             height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-          <!-- End Google Tag Manager (noscript) -->
+
         </body>
       </Html>
     )
